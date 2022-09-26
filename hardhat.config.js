8fd4950cf2fc82fbe1c56c0ae8ef7496a77f7054
levblanc <levblanc@gmail.com>
2022-09-26 10:36:30 +0800

    project init

---
+++ hardhat.config.js
@@ -0,0 +1,93 @@
+require('@nomicfoundation/hardhat-toolbox');
+require('hardhat-deploy');
+require('hardhat-contract-sizer');
+require('dotenv').config();
+
+const {
+  RINKEBY_RPC_URL,
+  GOERLI_RPC_URL,
+  ETHEREUM_MAINNET_RPC_URL,
+  PRIVATE_KEY,
+  ETHERSCAN_API_KEY,
+} = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      { version: '0.8.10' },
+      { version: '0.8.8' },
+      { version: '0.8.4' },
+      { version: '0.6.12' },
+      { version: '0.4.19' },
+    ],
+  },
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+      forking: {
+        // url: GOERLI_RPC_URL,
+        url: ETHEREUM_MAINNET_RPC_URL,
+      },
+    },
+    localhost: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    rinkeby: {
+      chainId: 4,
+      blockConfirmations: 6,
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+    goerli: {
+      chainId: 5,
+      blockConfirmations: 6,
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+    localhost: {
+      chainId: 31137,
+      url: 'http://127.0.0.1:8545',
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      // 1: 0,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+    customChains: [
+      {
+        network: 'rinkeby',
+        chainId: 4,
+        urls: {
+          apiURL: 'http://api-rinkeby.etherscan.io/api',
+          browserURL: 'https://rinkeby.etherscan.io',
+        },
+      },
+      {
+        network: 'goerli',
+        chainId: 5,
+        urls: {
+          apiURL: 'http://api-goerli.etherscan.io/api',
+          browserURL: 'https://goerli.etherscan.io',
+        },
+      },
+    ],
+  },
+  gasReporter: {
+    enabled: false, // set to true when needs a report
+    outputFile: 'gas-report.md',
+    noColors: true,
+    // currency: 'USD',
+    token: 'MATIC',
+  },
+  mocha: {
+    timeout: 2000000, // ms
+  },
+};
---
+++ helper-hardhat-config.js
@@ -0,0 +1,31 @@
+const { ethers } = require('hardhat');
+
+const networkConfig = {
+  31337: {
+    name: 'hardhat',
+    // Ethereum mainnet WETH token, forking mainnet on local hardhat
+    wethTokenAddress: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    // Ethereum mainnet deployed contract address:
+    // https://docs.aave.com/developers/v/2.0/deployed-contracts/deployed-contracts
+    poolAddressesProvider: '0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5',
+    aggregatorV3InterfaceAddress: '0x773616E4d11A78F511299002da57A0a94577F1f4',
+    daiAddress: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+  },
+  5: {
+    name: 'goerli',
+    wethTokenAddress: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
+    // Goerli testnet deployed contract address:
+    // https://docs.aave.com/developers/v/2.0/deployed-contracts/deployed-contracts
+    poolAddressesProvider: '0x5E52dEc931FFb32f609681B8438A51c675cc232d',
+    // DAI / USD
+    aggregatorV3InterfaceAddress: '0x0d79df66BE487753B02D015Fb622DED7f0E9798d',
+    daiAddress: '0x11fE4B6AE13d2a6055C8D9cF65c55bac32B5d844',
+  },
+};
+
+const developmentChains = ['hardhat', 'localhost'];
+
+module.exports = {
+  networkConfig,
+  developmentChains,
+};
